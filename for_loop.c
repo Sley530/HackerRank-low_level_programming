94bae4846ae9fa8ee8f759c1e6e0f9541550ee87
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-02-15 23:39:58 -0500

    Update

--- for_loop.c
+++ for_loop.c
@@ -5,21 +5,22 @@
 
 /*
  * main- program entry point
- * 
+ *
  * Description: This program will print the number n that is
  * in the intervall of [a, b]
  * it will print the nuber in english letters if n is in
  * the intervall 1 <= n <= 9
  * Otherwise it will print if n is 'even' or 'odd'
- * 
+ *
  * Return: (0) if successful
+ *
  */
 
-int main() 
+int main()
 {
     int a, b;
     scanf("%d\n%d", &a, &b);
-  	
+
 
     for (int n = a; n <= b; n++)
     {
